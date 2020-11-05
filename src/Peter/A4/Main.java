1cb000c5fb7072932635f6287439e8fd6dd40ebe
re-vc <frederikrehovic@gmail.com>
2020-11-05 15:19:48 +0100

    15:13 state

--- src/Peter/A4/Main.java
+++ src/Peter/A4/Main.java
@@ -11,17 +11,32 @@ public class Main {
                 3,
                 33.33,
                 1.15);
+        Invoice inv01 = new Invoice(
+                "INVC-001",
+                "this is the total for your 2nd order.",
+                99,
+                249.13,
+                1.0);
 
         double invT = inv.getTotal();
+        double invT2 = inv01.getTotal();
         double roundOffinvT = Math.round(invT * 100.0) / 100.0;
+        double roundOffinvT2 = Math.rint(invT2 * 100.0) / 100.0;
 
         System.out.println(inv);
 
         if(inv.price < 0){
             inv.price = 0;
-            System.out.println("your total is: " + inv.price);
+            System.out.println("your total is: € " + inv.price);
         } else {
-            System.out.println("your total is: " + roundOffinvT);
+            System.out.println("your total is: € " + roundOffinvT);
+        }
+
+        if(inv01.price < 0){
+            inv01.price = 0;
+            System.out.println("your total is: € " + inv01.price);
+        } else {
+            System.out.println("your total is: € " + roundOffinvT2);
         }
     }
 }
